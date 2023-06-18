10f5c34c80241fcae00667079a9c0411eab30870
DESKTOP-2CCSS00\Max <maxweijers@hotmail.nl>
2023-06-18 20:27:13 +0200

    movement works

--- ProjectVrijTeamZes/Assets/Guest.cs
+++ ProjectVrijTeamZes/Assets/Guest.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Guest : MonoBehaviour
 {
-    public List<Sprite> images;
+    public List<Material> images;
     public float moveSpeed;
 
     public GameObject previousPoint, nextPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        int random = Random.Range(0, images.Count);
+        transform.GetChild(0).GetComponent<MeshRenderer>().material = images[random];
+        GetNextPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, nextPoint.transform.position, moveSpeed * Time.deltaTime);
+        if(Vector3.Distance(transform.position, nextPoint.transform.position) < 0.1f) {
+            //arrived
+            previousPoint = nextPoint;
+            GetNextPoint();
+        }
     }
 
     public void GetNextPoint() {
